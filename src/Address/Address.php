2efc409391d96a8a140d9f5ec06be2e300beeeec
Junior Grossi <juniorgro@gmail.com>
2016-07-29 17:53:22 -0300

    Add basic country support for the general Address class

--- src/Address/Address.php
+++ src/Address/Address.php
@@ -4,8 +4,57 @@ namespace InterNACHI\Address;
 
 class Address
 {
-	public function __construct()
+	/**
+	 * @var string|null
+	 */
+	protected $locale;
+
+	/**
+	 * @var Country
+	 */
+	protected $country;
+
+	/**
+	 * Constructor method
+	 *
+	 * @param string|null $locale
+	 */
+	public function __construct($locale = null)
 	{
-		
+		$this->locale = $locale;
+		$this->country = new Country;
+		$this->country->setLocale($locale);
+	}
+
+	/**
+	 * Get the country list
+	 *
+	 * @return ArrayObject
+	 */
+	public function getCountries()
+	{
+		return $this->country->getAll();
+	}
+
+	/**
+	 * Get a country by code
+	 *
+	 * @param $code
+	 * @return Country|null
+	 */
+	public function findCountryByCode($code)
+	{
+		return $this->country->findByCode($code);
+	}
+
+	/**
+	 * Get a country by name
+	 *
+	 * @param $name
+	 * @return Country|null
+	 */
+	public function findCountryByName($name)
+	{
+		return $this->country->findByName($name);
 	}
 }
