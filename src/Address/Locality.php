dd09dcff17d35f843323ec1b4f1a0728a802fe80
Junior Grossi <juniorgro@gmail.com>
2016-07-29 18:03:37 -0300

    First changes to get localities from an Administrative Area

--- src/Address/Locality.php
+++ src/Address/Locality.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace InterNACHI\Address;
+use ArrayObject;
+use CommerceGuys\Addressing\Collection\LazySubdivisionCollection;
+use Illuminate\Support\Arr;
 
 /**
  * Class Locality
@@ -10,5 +13,36 @@ namespace InterNACHI\Address;
  */
 class Locality
 {
+	/**
+	 * @var AdministrativeArea
+	 */
+	protected $administrativeArea;
 
+	/**
+	 * The constructor method
+	 *
+	 * @param AdministrativeArea $administrativeArea
+	 */
+	public function __construct(AdministrativeArea $administrativeArea)
+	{
+		$this->administrativeArea = $administrativeArea;
+	}
+
+	/**
+	 * Get all localities from a given Administrative Area
+	 *
+	 * @return ArrayObject
+	 */
+	public function getAll()
+	{
+		$subdivision = $this->administrativeArea->getSubdivision();
+		$list = new ArrayObject();
+		$children = $subdivision->getChildren();
+		/** @var LazySubdivisionCollection $child */
+		foreach ($children as $child) {
+			$locality = new static($this->administrativeArea);
+			// TODO
+			$list->append($locality);
+		}
+	}
 }
