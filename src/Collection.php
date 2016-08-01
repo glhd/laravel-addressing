44d0e54f3a4da11de037294522faf9ade158051c
Junior Grossi <juniorgro@gmail.com>
2016-08-01 16:10:51 -0300

    Add a new Collection class implementing the IteratorAggregate interface

---
+++ src/Collection.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Galahad\LaravelAddressing;
+
+use ArrayObject;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Class Collection
+ *
+ * @package Galahad\LaravelAddressing
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class Collection implements IteratorAggregate
+{
+    /**
+     * @var ArrayObject
+     */
+    private $items;
+
+    /**
+     * The construct method
+     */
+    public function __construct()
+    {
+        $this->items = new ArrayObject();
+    }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator()
+    {
+        return new Iterator($this->items);
+    }
+
+    /**
+     * Insert a new element in the collection
+     *
+     * @param $value
+     */
+    public function insert($value)
+    {
+        $this->items->append($value);
+    }
+
+    /**
+     * Get the items count
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return $this->items->count();
+    }
+}
