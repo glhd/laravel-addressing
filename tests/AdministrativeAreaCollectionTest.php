dc3b2790b8f491c600c175ec0d79c2af86f54523
Junior Grossi <juniorgro@gmail.com>
2016-08-01 18:35:01 -0300

    Add new test class for Administrative Area collections

---
+++ tests/AdministrativeAreaCollectionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Galahad\LaravelAddressing\AdministrativeAreaCollection;
+use Galahad\LaravelAddressing\Country;
+
+/**
+ * Class AdministrativeAreaCollectionTest
+ *
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class AdministrativeAreaCollectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testCollectionClass()
+    {
+        $country = new Country;
+        $brazil = $country->findByCode('BR');
+        $states = $brazil->getAdministrativeAreas();
+
+        $this->assertInstanceOf(AdministrativeAreaCollection::class, $states);
+    }
+
+    public function testUSStatesCount()
+    {
+        $country = new Country;
+        $us = $country->findByCode('US');
+        $states = $us->getAdministrativeAreas();
+
+        $this->assertEquals($states->count(), 62);
+    }
+}
