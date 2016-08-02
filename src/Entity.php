b3ac553cc1cb798a5c8793fdb8ffc4fa6c923fb8
Junior Grossi <juniorgro@gmail.com>
2016-08-02 16:45:13 -0300

    Add Entity class with common methods

---
+++ src/Entity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Galahad\LaravelAddressing;
+
+/**
+ * Class Entity
+ *
+ * @package Galahad\LaravelAddressing
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class Entity
+{
+    /**
+     * Get property if get method exists
+     *
+     * @param $name
+     * @return mixed
+     */
+    function __get($name)
+    {
+        $name = array_walk(explode('_', $name), 'ucfirst'); // some_name => ['Some', 'Name']
+        $name = implode('', $name); // SomeName
+        $method = 'get'.$name;
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+}
