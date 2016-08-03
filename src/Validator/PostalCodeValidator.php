f39ed77ccfd155b423421041be367a7b587ab411
Junior Grossi <juniorgro@gmail.com>
2016-08-03 18:00:55 -0300

    Add postal code validator

--- src/Validator/PostalCodeValidator.php
+++ src/Validator/PostalCodeValidator.php
@@ -2,6 +2,9 @@
 
 namespace Galahad\LaravelAddressing\Validator;
 
+use Galahad\LaravelAddressing\Entity\AdministrativeArea;
+use Galahad\LaravelAddressing\Entity\Country;
+use Galahad\LaravelAddressing\LaravelAddressing;
 use Illuminate\Validation\Validator;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -13,6 +16,18 @@ use Symfony\Component\Translation\TranslatorInterface;
  */
 class PostalCodeValidator extends Validator
 {
+    /**
+     * @var array
+     */
+    protected $messages = [
+        'postal_code' => 'The :attribute seems do be a not valid postal code',
+    ];
+
+    /**
+     * @var LaravelAddressing
+     */
+    protected $addressing;
+
     /**
      * @param TranslatorInterface $translator
      * @param array $data
@@ -28,5 +43,32 @@ class PostalCodeValidator extends Validator
         array $customAttributes = []
     ) {
         parent::__construct($translator, $data, $rules, $messages, $customAttributes);
+        $this->addressing = new LaravelAddressing();
+        $this->setCustomMessages($this->messages);
+    }
+
+    /**
+     * Validate a postal code using country and administrative area fields
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @param array $parameters
+     * @return bool|int
+     */
+    protected function validatePostalCode($attribute, $value, $parameters)
+    {
+        $this->requireParameterCount(2, $parameters, 'postal_code');
+        $country = $this->addressing->getCountryByCodeOrName($this->getValue($parameters[0]));
+        if ($country instanceof Country) {
+            $admAreaValue = $this->getValue($parameters[1]);
+            $admArea = $country->getAdministrativeAreas()->getByCodeOrName($admAreaValue);
+            if ($admArea instanceof AdministrativeArea) {
+                $postalCodePattern = $admArea->getPostalCodePattern();
+
+                return preg_match("/^$postalCodePattern/", $value);
+            }
+        }
+
+        return false;
     }
 }
