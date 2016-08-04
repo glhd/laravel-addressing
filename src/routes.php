2adc2888147b158999f9c8c779c8c3af01378690
Junior Grossi <juniorgro@gmail.com>
2016-08-04 12:09:51 -0300

    Add route for getting all countries

--- src/routes.php
+++ src/routes.php
@@ -17,7 +17,12 @@ Route::group(['prefix' => 'galahad/addressing'], function() {
 
 	Route::get('/{country}/administrative-areas', [
 		'as' => 'galahad.addressing.administrative-areas',
-		'uses' => '\\Galahad\\LaravelAddressing\\Controller@getAdministrativeAreas'
+		'uses' => '\\Galahad\\LaravelAddressing\\Controller@getAdministrativeAreas',
 	]);
+
+    Route::get('/countries', [
+        'as' => 'galahad.addressing.countries',
+        'uses' => '\\Galahad\\LaravelAddressing\\Controller@getCountries',
+    ]);
 
 });
