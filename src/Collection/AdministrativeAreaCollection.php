38f629436f12e4c1dde880bbde86c5f5cdbebcd0
Junior Grossi <juniorgro@gmail.com>
2016-08-08 10:17:52 -0300

    Add AdministrativeAreaCollection class extending from Laravel one

---
+++ src/Collection/AdministrativeAreaCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Galahad\LaravelAddressing\Collection;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Class AdministrativeAreaCollection
+ *
+ * @package Galahad\LaravelAddressing\Collection
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class AdministrativeAreaCollection extends Collection
+{
+
+}
