94eecd33067430d54d19360fbcb8f41cda567d7c
Junior Grossi <juniorgro@gmail.com>
2016-08-08 10:27:17 -0300

    Make Administrative Area Repository returning instance of AdministrativeArea

--- src/Repository/AdministrativeAreaRepository.php
+++ src/Repository/AdministrativeAreaRepository.php
@@ -1,6 +1,86 @@
 <?php
 
-class SubdivisionRepository extends \CommerceGuys\Addressing\Repository\SubdivisionRepository
+namespace Galahad\LaravelAddressing\Repository;
+
+use Closure;
+use CommerceGuys\Addressing\Collection\LazySubdivisionCollection;
+use CommerceGuys\Addressing\Repository\SubdivisionRepository;
+use Galahad\LaravelAddressing\Collection\AdministrativeAreaCollection;
+use Galahad\LaravelAddressing\Entity\AdministrativeArea;
+
+/**
+ * Class AdministrativeAreaRepository
+ *
+ * @package Galahad\LaravelAddressing\Repository
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class AdministrativeAreaRepository extends SubdivisionRepository
 {
-	
+    /**
+     * Create Subdivisions using the custom AdministrativeArea class
+     *
+     * @param int $id
+     * @param array $definitions
+     * @param string $locale
+     * @return AdministrativeArea|null
+     */
+    protected function createSubdivisionFromDefinitions($id, array $definitions, $locale)
+    {
+        if (!isset($definitions['subdivisions'][$id])) {
+            // No matching definition found.
+            return null;
+        }
+
+        $definition = $this->translateDefinition($definitions['subdivisions'][$id], $locale);
+        // Add common keys from the root level.
+        $definition['country_code'] = $definitions['country_code'];
+        $definition['parent_id'] = $definitions['parent_id'];
+        $definition['locale'] = $definitions['locale'];
+        // Provide defaults.
+        if (!isset($definition['code'])) {
+            $definition['code'] = $definition['name'];
+        }
+        // Load the parent, if known.
+        $definition['parent'] = null;
+        if (isset($definition['parent_id'])) {
+            $parentId = $definition['parent_id'];
+            if (!isset($this->parents[$parentId])) {
+                $this->parents[$parentId] = $this->get($definition['parent_id']);
+            }
+            $definition['parent'] = $this->parents[$parentId];
+        }
+
+        $subdivision = new AdministrativeArea();
+        // Bind the closure to the Subdivision object, giving it access to its
+        // protected properties. Faster than both setters and reflection.
+        $setValues = Closure::bind(function ($id, $definition) {
+            $this->parent = $definition['parent'];
+            $this->countryCode = $definition['country_code'];
+            $this->id = $id;
+            $this->code = $definition['code'];
+            $this->name = $definition['name'];
+            $this->locale = $definition['locale'];
+            if (isset($definition['postal_code_pattern'])) {
+                $this->postalCodePattern = $definition['postal_code_pattern'];
+                if (isset($definition['postal_code_pattern_type'])) {
+                    $this->postalCodePatternType = $definition['postal_code_pattern_type'];
+                } else {
+                    $this->postalCodePatternType = PatternType::getDefault();
+                }
+            }
+        }, $subdivision, '\Galahad\LaravelAddressing\Entity\AdministrativeArea');
+        $setValues($id, $definition);
+
+        if (!empty($definition['has_children'])) {
+            $children = new AdministrativeAreaCollection();
+            $children->setCountryCode($definition['country_code']);
+            $children->setParentId($id);
+            $children->setLocale($definition['locale']);
+            $children->setSubdivisionRepository($this);
+            $subdivision->setChildren($children);
+        }
+
+        return $subdivision;
+    }
+
 }
