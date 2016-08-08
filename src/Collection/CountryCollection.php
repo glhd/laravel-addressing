38fe965a7ffb5d4b79a9112765bc67e4db68ed31
Junior Grossi <juniorgro@gmail.com>
2016-08-08 10:00:21 -0300

    Base CountryCollection class

---
+++ src/Collection/CountryCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Galahad\LaravelAddressing\Collection;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Class CountryCollection
+ *
+ * @package Galahad\LaravelAddressing\Collection
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class CountryCollection extends Collection
+{
+
+}
