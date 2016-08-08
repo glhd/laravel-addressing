536d125d2ff682989f0836519771645f07181982
Junior Grossi <juniorgro@gmail.com>
2016-08-08 10:21:20 -0300

    Add custom logic to AdministrativeAreaCollection class

--- src/Collection/AdministrativeAreaCollection.php
+++ src/Collection/AdministrativeAreaCollection.php
@@ -12,5 +12,66 @@ use Illuminate\Support\Collection;
  */
 class AdministrativeAreaCollection extends Collection
 {
+    /**
+     * @var array|mixed
+     */
+    protected $countryCode;
 
+    /**
+     * @var string
+     */
+    protected $parentId;
+
+    /**
+     * @var string|null
+     */
+    protected $locale = null;
+
+    /**
+     * @return array|mixed
+     */
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param array|mixed $countryCode
+     */
+    public function setCountryCode($countryCode)
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param mixed $parentId
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
+    }
+
+    /**
+     * @return null
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param null $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
 }
