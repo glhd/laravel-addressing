3e8bada266df8140f4e5431cba0dfb72502ece24
Junior Grossi <juniorgro@gmail.com>
2016-08-08 13:10:57 -0300

    Change administrative area validator class to match the new structure

--- src/Validator/AdministrativeAreaValidator.php
+++ src/Validator/AdministrativeAreaValidator.php
@@ -48,7 +48,7 @@ class AdministrativeAreaValidator extends Validator
     ) {
         parent::__construct($translator, $data, $rules, $messages, $customAttributes);
         $this->setCustomMessages($this->messages);
-        $this->addressing = new LaravelAddressing(); // TODO: Shouldn't this load from the service locator?
+        $this->addressing = LaravelAddressing::getInstance();
     }
 
     /**
@@ -63,7 +63,7 @@ class AdministrativeAreaValidator extends Validator
     {
         $this->requireParameterCount(1, $parameters, 'administrative_area_code');
         $country = $this->getCountryInstance($parameters);
-        $admArea = $country->getAdministrativeAreas()->getByCode($value);
+        $admArea = $country->administrativeArea($value);
 
         return $admArea instanceof AdministrativeArea;
     }
@@ -80,7 +80,7 @@ class AdministrativeAreaValidator extends Validator
     {
         $this->requireParameterCount(1, $parameters, 'administrative_area_name');
         $country = $this->getCountryInstance($parameters);
-        $admArea = $country->getAdministrativeAreas()->getByName($value);
+        $admArea = $country->administrativeAreaByName($value);
 
         return $admArea instanceof AdministrativeArea;
     }
@@ -111,12 +111,8 @@ class AdministrativeAreaValidator extends Validator
      */
     private function getCountryInstance(array $parameters)
     {
-        $countryCode = $this->getValue($parameters[0]);
-        $country = $this->addressing->getCountryByCode($countryCode); // TODO: I think there's a method that already does this logic
-        if (is_null($country)) {
-            $country = $this->addressing->getCountryByName($countryCode);
-            return $country;
-        }
-        return $country;
+        $countryCodeOrName = $this->getValue($parameters[0]);
+
+        return $this->addressing->findCountry($countryCodeOrName);
     }
 }
