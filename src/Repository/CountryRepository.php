c13d20371c346df6c5603c8a2dc029b5ce362f97
Junior Grossi <juniorgro@gmail.com>
2016-08-08 09:40:59 -0300

    Basic CountryRepository structure

--- src/Repository/CountryRepository.php
+++ src/Repository/CountryRepository.php
@@ -1,32 +1,16 @@
 <?php
 
-class CountryRepository extends \CommerceGuys\Intl\Country\CountryRepository
+namespace Galahad\LaravelAddressing\Repository;
+
+use CommerceGuys\Addressing\Repository\CountryRepository as BaseCountryRepository;
+
+/**
+ * Class CountryRepository
+ *
+ * @package Galahad\LaravelAddressing\Repository
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class CountryRepository extends BaseCountryRepository
 {
-	protected function createCountryFromDefinition($countryCode, array $definition, $locale)
-	{
-		$country = new \Galahad\Addressing\Country();
-		$setValues = \Closure::bind(function ($countryCode, $definition, $locale) {
-			$this->countryCode = $countryCode;
-			$this->name = $definition['name'];
-			$this->locale = $locale;
-			if (isset($definition['three_letter_code'])) {
-				$this->threeLetterCode = $definition['three_letter_code'];
-			}
-			if (isset($definition['numeric_code'])) {
-				$this->numericCode = $definition['numeric_code'];
-			}
-			if (isset($definition['currency_code'])) {
-				$this->currencyCode = $definition['currency_code'];
-			}
-		}, $country, '\CommerceGuys\Intl\Country\Country');
-		$setValues($countryCode, $definition, $locale);
-		
-		return $country;
-	}
-	
-	public function getAll($locale = null, $fallbackLocale = null)
-	{
-		$countries = parent::getAll($locale, $fallbackLocale);
-		return new Collection($countries);
-	}
+    
 }
