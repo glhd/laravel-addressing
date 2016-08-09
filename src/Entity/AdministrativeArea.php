6f08f2b8f28ea4ae1e89fd9a1750d95a598a1509
Junior Grossi <juniorgro@gmail.com>
2016-08-09 11:01:24 -0300

    Clean the code

--- src/Entity/AdministrativeArea.php
+++ src/Entity/AdministrativeArea.php
@@ -13,21 +13,21 @@ use CommerceGuys\Addressing\Model\Subdivision;
  */
 class AdministrativeArea extends Subdivision
 {
+    /**
+     * @param Subdivision $subdivision
+     */
     public function __construct(Subdivision $subdivision)
     {
         $this->copyPropertiesFromBaseClass($subdivision);
     }
 
+    /**
+     * @param Subdivision $subdivision
+     */
     protected function copyPropertiesFromBaseClass(Subdivision $subdivision)
     {
         foreach (get_object_vars($subdivision) as $key => $value) {
             $this->$key = $value;
         }
-//        if ($subdivision->hasChildren()) {
-//            /** @var LazySubdivisionCollection $children */
-//            $children = $subdivision->getChildren();
-//            $children->setRepository($this);
-//            $this->setChildren($children);
-//        }
     }
 }
