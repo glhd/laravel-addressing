b607a866785fb3f2ae4c942a45358a70ebde27e7
Junior Grossi <juniorgro@gmail.com>
2016-08-12 16:54:39 -0300

    Add unit tests for translated validation messages in en and pt-br

---
+++ tests/TranslationTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Galahad\LaravelAddressing\ServiceProvider;
+use Illuminate\Validation\Factory;
+use Orchestra\Testbench\TestCase;
+
+/**
+ * Class TranslationTest
+ *
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class TranslationTest extends TestCase
+{
+    /**
+     * @var Factory
+     */
+    protected $validator;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->validator = $this->app['validator'];
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [ServiceProvider::class];
+    }
+
+    public function testENMessages()
+    {
+        $this->app->setLocale('en');
+        $validator = $this->validator->make(
+            ['country' => 'ZZ'],
+            ['country' => 'country_code']
+        );
+        if ($validator->fails()) {
+            $messages = $validator->errors()->get('country');
+            $first = array_shift($messages);
+            $this->assertEquals($first, 'The country has not a valid country code.');
+        }
+    }
+
+    public function testPT_BRMessages()
+    {
+        $this->app->setLocale('pt-br');
+        $validator = $this->validator->make(
+            ['country' => 'ZZ'],
+            ['country' => 'country_code']
+        );
+        if ($validator->fails()) {
+            $messages = $validator->errors()->get('country');
+            $first = array_shift($messages);
+            $this->assertEquals($first, 'O campo country não possui um código de país válido.');
+        }
+    }
+
+    public function testENMessagesForAdministrativeAreaValidator()
+    {
+        $this->app->setLocale('en');
+        $validator = $this->validator->make(
+            ['country' => 'US', 'state' => 'COO'],
+            ['country' => 'country_code', 'state' => 'administrative_area:country']
+        );
+        if ($validator->fails()) {
+            $messages = $validator->errors()->get('state');
+            $first = array_shift($messages);
+            $this->assertEquals($first, 'The state has not a valid administrative area name.');
+        }
+    }
+}
